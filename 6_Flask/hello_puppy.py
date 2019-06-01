06492278571fd6761b2ae12b08cf1fc98926c25a
bdemin <42113340+bdemin@users.noreply.github.com>
2019-06-01 21:55:06 +0300

    Added another route decorator

--- 6_Flask/hello_puppy.py
+++ 6_Flask/hello_puppy.py
@@ -3,9 +3,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #127.0.0.1:5000
 def index():
     return '<h1>Hello Puppy!</h1>'
 
+
+@app.route('/information')
+def info():
+    return "<h1>Here is a different route</h1>"
 if __name__ == '__main__':
     app.run()
